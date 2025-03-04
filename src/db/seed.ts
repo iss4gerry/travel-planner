25a4d27d0cf8aad158b95393c798f02c651db77d
“gerry” <gerrydesrian488@gmail.com>
2025-03-04 16:45:47 +0700

    feat: db schema and seed

---
+++ src/db/seed.ts
@@ -0,0 +1,25 @@
+import { prisma } from '../db/index';
+
+const categories = [
+	{ name: 'nature', imageUrl: 'example' },
+	{ name: 'adventure', imageUrl: 'example' },
+	{ name: 'other', imageUrl: 'example' },
+	{ name: 'culinary', imageUrl: 'example' },
+];
+
+async function seedDatabase() {
+	try {
+		await prisma.category.deleteMany();
+
+		await prisma.category.createMany({
+			data: categories,
+			skipDuplicates: true,
+		});
+	} catch (error) {
+		console.error('❌ Error seeding database:', error);
+	} finally {
+		await prisma.$disconnect();
+	}
+}
+
+seedDatabase();
