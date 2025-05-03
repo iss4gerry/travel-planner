a1047cfc06319cd4ca9ea77927b4e700cbb15cfa
iss4gerry <gerrydesrian488@gmail.com>
2025-05-03 19:44:27 +0700

    fix: fetch banner and destination by userId

--- src/components/Dashboard/HorizontalCard.tsx
+++ src/components/Dashboard/HorizontalCard.tsx
@@ -5,7 +5,7 @@ import {
 import { BannerResponse } from '@/types/banner';
 import { DestinationResponse } from '@/types/destination';
 import { useQuery } from '@tanstack/react-query';
-import { DollarSign, MapIcon, MapPin, Wallet, Waypoints } from 'lucide-react';
+import { MapPin, Wallet } from 'lucide-react';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
@@ -29,20 +29,39 @@ export default function HorizontalCard({ mode }: { mode: string }) {
 	if (loadingDestination || loadingBanner) {
 		return <CardSkeleton />;
 	}
-
-	console.log(bannerData);
+	console.log(destinationData);
 
 	return (
 		<div>
-			{!bannerData || !destinationData ? (
-				<div className="flex items-center justify-center w-full h-80">
-					<p>You haven't created any {mode} yet.</p>
-				</div>
-			) : (
+			{mode === 'destination' && (
 				<div>
-					{mode === 'destination'
-						? destinationData.map((d) => <Card mode="destination" data={d} />)
-						: bannerData.map((d) => <Card mode="banner" data={d} />)}
+					{!destinationData || destinationData.length === 0 ? (
+						<div className="flex items-center justify-center w-full h-80">
+							<p>You haven't created any destinations yet.</p>
+						</div>
+					) : (
+						<div>
+							{destinationData.map((d, index) => (
+								<Card key={index} mode="destination" data={d} />
+							))}
+						</div>
+					)}
+				</div>
+			)}
+
+			{mode === 'banner' && (
+				<div>
+					{!bannerData || bannerData.length === 0 ? (
+						<div className="flex items-center justify-center w-full h-80">
+							<p>You haven't created any banners yet.</p>
+						</div>
+					) : (
+						<div>
+							{bannerData.map((d, index) => (
+								<Card key={index} mode="banner" data={d} />
+							))}
+						</div>
+					)}
 				</div>
 			)}
 		</div>
@@ -62,7 +81,7 @@ function Card({
 	};
 	return (
 		<div
-			className="flex flex-row w-full justify-start bg-white p-2 border-b border-base-200 hover:bg-base-100 hover:cursor-pointer"
+			className="flex flex-row w-full justify-start bg-base-100 p-2 border-b border-base-200 hover:bg-base-200 hover:cursor-pointer"
 			onClick={handleClick}
 		>
 			<div className="relative w-40 h-40 overflow-hidden rounded-md shadow-md">
@@ -81,7 +100,8 @@ function Card({
 			<div className="flex flex-col pl-4 justify-between">
 				<div>
 					<h1 className="text-2xl font-semibold text-gray-800 truncate">
-						(data as DestinationResponse).name || (data as BannerResponse).title
+						{(data as DestinationResponse).name ||
+							(data as BannerResponse).title}
 					</h1>
 					<p className="text-sm text-gray-600 line-clamp-2 mt-2">
 						{data.description ||
@@ -102,11 +122,7 @@ function Card({
 				<div className="mt-3 text-sm text-gray-500">
 					<p>
 						<span className="font-semibold">Created At:</span>{' '}
-						{data.createdAt
-							.toString()
-							.split(' ')
-							.filter((_, index) => index === 1 || index === 2 || index === 3)
-							.join(' ')}
+						{data.createdAt.toString().split('T')[0]}
 					</p>
 				</div>
 			</div>
